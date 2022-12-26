f6f358c62af67d0a5f02cb1300c63bf7bdcbc250
sidiqadi7 <sidiqadi7@gmail.com>
2022-12-27 00:00:10 +0700

    test_1

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -28,31 +28,94 @@ class DebitCardTransactionControllerTest extends TestCase
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
         // get /debit-card-transactions
+        $response = $this->get('/debit-card-transactions')
+                ->assertJsonStructure([
+                    'status',
+                    'result' => [
+                        'debit_card_id',
+                        'amount',
+                        'currency_code',
+                    ],
+                ]);
+
+        $response->assertOk();
+        
     }
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
     {
         // get /debit-card-transactions
+        $response = $this->get('/debit-card-transactions');
+                
+        $response->assertTrue($this->debitCards->has('debitCardTransactions'));
     }
 
     public function testCustomerCanCreateADebitCardTransaction()
     {
         // post /debit-card-transactions
+        $response = $this->post('/debit-card-transactions', [
+            'debit_card_id' => 1,
+            'amount' => 2000,
+            'currency_code' => 'IDR',
+        ]);
+
+        $newDebitCardTransaction = (array)json_decode($response->response->content());
+
+        $response->assertArrayHasKey('debit_card_id', $newDebitCardTransaction);
+        $response->assertCount(
+            1,
+            $this->debitCards->debitCardTransaction->latest()->count(), "1 data has been added"
+        );
+        $response->assertEquals($newDebitCardTransaction['debit_card_id'], $this->debitCards->debitCardTransaction->get());
     }
 
     public function testCustomerCannotCreateADebitCardTransactionToOtherCustomerDebitCard()
     {
         // post /debit-card-transactions
+        $response = $this->post('/debit-card-transactions', [
+            'debit_card_id' => 1,
+            'amount' => 2000,
+            'currency_code' => 'IDR',
+        ]);
+
+        $newDebitCardTransaction = $response->request->getContent();
+        $getDebitCard = $this->debitCard->where(['id'=> $newDebitCardTransaction['debit_card_id']])->get();
+
+        $response->assertValid(['debit_card_id']);
+        $response->assertEquals($this->user->id, $getDebitCard['user_id']);
     }
 
     public function testCustomerCanSeeADebitCardTransaction()
     {
         // get /debit-card-transactions/{debitCardTransaction}
+        $debitCardTransactionId = 1;
+        $response = $this->get('/debit-card-transactions', [
+            'id' => $debitCardTransactionId,
+        ])->assertJsonStructure([
+            'status',
+            'result' => [
+                'debit_card_id',
+                'amount',
+                'currency_code',
+            ],
+        ]);
+        
+        $response->assertSuccessful();
     }
 
     public function testCustomerCannotSeeADebitCardTransactionAttachedToOtherCustomerDebitCard()
     {
         // get /debit-card-transactions/{debitCardTransaction}
+        $debitCardTransactionId = 1;
+        $response = $this->get('/debit-card-transactions', [
+            'id' => $debitCardTransactionId,
+        ]);
+        
+        $debitCardTransaction = (array)json_decode($response->response->content());
+        $getDebitCard = $this->debitCard->where(['id'=> $newDebitCardTransaction['debit_card_id']])->get();
+
+        $response->assertSuccessful();
+        $response->assertEquals($this->user->id, $getDebitCard['user_id']);
     }
 
     // Extra bonus for extra tests :)
