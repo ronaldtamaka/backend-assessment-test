5f134dfca1d9f522fbaca279b2160821d87034ef
Dihak <dihak@digitalkode.com>
2023-01-04 21:49:52 +0700

    feat(loan): create createLoan function

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -21,7 +21,39 @@ class LoanService
      */
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
-        //
+        $loan = Loan::create([
+            'user_id' => $user->id,
+            'amount' => $amount,
+            'currency_code' => $currencyCode,
+            'terms' => $terms,
+            'outstanding_amount' => $amount,
+            'processed_at' => $processedAt,
+            'status' => Loan::STATUS_DUE,
+        ]);
+
+        // generate scheduled repayments
+        $scheduledRepayments = [];
+
+        for ($i = 1; $i <= $terms; $i++) {
+            $dueDate = date('Y-m-d', strtotime($processedAt . ' + ' . $i . ' month'));
+
+            $amountTerm = floor($amount / $terms);
+
+            if ($i === $terms) {
+                $amountTerm = $amount - ($amountTerm * ($terms - 1));
+            }
+
+            $scheduledRepayments[] = [
+                'amount' => $amountTerm,
+                'outstanding_amount' => $amountTerm,
+                'currency_code' => $currencyCode,
+                'due_date' => $dueDate,
+            ];
+        }
+
+        $loan->scheduledRepayments()->createMany($scheduledRepayments);
+
+        return $loan;
     }
 
     /**
