b3223999438bc98d36c697f7a28d5fb0f346c457
manbudi5900 <manbudi590@gmail.com>
2023-09-24 12:49:26 +0800

    make scnario customer can see a list of debit card transaction

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
+use Carbon\Carbon;
+
 
 class DebitCardTransactionControllerTest extends TestCase
 {
@@ -28,6 +30,21 @@ class DebitCardTransactionControllerTest extends TestCase
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
         // get /debit-card-transactions
+        $debitCard = DebitCard::create([
+            'type' => "Gold",
+            'user_id'=> $this->user->id,
+            'number' => rand(1000000000000000, 9999999999999999),
+            'expiration_date' => Carbon::now()->addYear(),
+        ]);
+        $debitCardTransaction = $debitCard->debitCardTransactions()->create([
+            'amount' => 200000,
+            'currency_code' => "coba coba",
+        ]);
+
+
+        $response = $this->get('/api/debit-card-transactions?debit_card_id='.$debitCard->id);
+
+        $response->assertStatus(200);
     }
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
