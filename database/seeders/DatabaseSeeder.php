2e40addaf4ad83959d3af943af9b8c979e572914
Iyon AdryanLF <adryaniyon@gmail.com>
2023-09-27 01:44:36 +0700

    Fix Migration, Namespace, And Add Auth For Login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'email_verified_at'   => date("Y-m-d H:i:s"),
+            'password' => Hash::make('password'),
+            'remember_token'        => Str::random(10),
+        ]);
     }
 }
