57190858359c5e0f0e2f93e50a44a965a6f12d92
Ardhi Ramadhan <ardhiramadhan98@gmail.com>
2024-01-18 17:30:11 +0700

    test customer ability to view a list ot debit cards

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\DebitCard;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
@@ -23,11 +24,34 @@ class DebitCardControllerTest extends TestCase
     public function testCustomerCanSeeAListOfDebitCards()
     {
         // get /debit-cards
+
+        // Arrange
+        DebitCard::factory()->count(3)->create([
+            'disabled_at' => null,
+            'user_id' => $this->user->id,
+        ]);
+
+        // Act
+        $response = $this->get('/api/debit-cards');
+
+        // Assert
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            [
+                'id',
+                'number',
+                'type',
+                'expiration_date',
+                'is_active',
+            ],
+        ]);
+        $this->assertCount(3, $response->json());
     }
 
     public function testCustomerCannotSeeAListOfDebitCardsOfOtherCustomers()
     {
         // get /debit-cards
+
     }
 
     public function testCustomerCanCreateADebitCard()
