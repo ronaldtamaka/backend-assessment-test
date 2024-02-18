4802658e2c46b42ddcaefb93843530ded170df48
Candra Sudirman <dev.cndrsdrmn@gmail.com>
2024-02-19 06:23:52 +0700

    feat: implement service create loan

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -4,7 +4,10 @@ namespace App\Services;
 
 use App\Models\Loan;
 use App\Models\ReceivedRepayment;
+use App\Models\ScheduledRepayment;
 use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LoanService
 {
@@ -18,10 +21,34 @@ class LoanService
      * @param  string  $processedAt
      *
      * @return Loan
+     * @throws \Throwable
      */
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
-        //
+        DB::beginTransaction();
+        try {
+            /** @var Loan $loan */
+            $loan = Loan::create([
+                'user_id' => $user->id,
+                'amount' => $amount,
+                'terms' => $terms,
+                'outstanding_amount' => $amount,
+                'currency_code' => $currencyCode,
+                'processed_at' => $processedAt,
+                'status' => Loan::STATUS_DUE,
+            ]);
+
+            $loan->scheduledRepayments()->createMany(
+                $this->prepareSchedules($loan)
+            );
+
+            DB::commit();
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+
+        return $loan;
     }
 
     /**
@@ -37,5 +64,40 @@ class LoanService
     public function repayLoan(Loan $loan, int $amount, string $currencyCode, string $receivedAt): ReceivedRepayment
     {
         //
+    }
+
+    /**
+     * Prepare data of scheduled repayment.
+     *
+     * @param  Loan  $loan
+     * @return array
+     */
+    protected function prepareSchedules(Loan $loan): array
+    {
+        $schedules = [];
+        $totalAmount = $loan->amount;
+        $amountPerTerm = intdiv($loan->amount, $loan->terms);
+        $termStartedAt = $loan->processed_at;
+
+        for ($i = 1; $i <= $loan->terms; $i++) {
+            $dueDate = Carbon::parse($termStartedAt)->addMonth()->format('Y-m-d');
+            $termStartedAt = $dueDate;
+
+            if ($i === $loan->terms) {
+                $amountPerTerm = round($totalAmount);
+            }
+
+            $schedules[] = [
+                'amount' => $amountPerTerm,
+                'outstanding_amount' => $amountPerTerm,
+                'currency_code' => $loan->currency_code,
+                'due_date' => $dueDate,
+                'status' => ScheduledRepayment::STATUS_DUE,
+            ];
+
+            $totalAmount -= $amountPerTerm;
+        }
+
+        return $schedules;
     }
 }
